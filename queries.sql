f0fcf3f98b418a881da235b803c6c9e4ee460413
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-27 19:55:13 -0600

    Completed second query

--- queries.sql
+++ queries.sql
@@ -11,6 +11,14 @@
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+    SELECT 
+        o."Id" "OrderId", 
+        s."CompanyName" "ShipperCompanyName"
+    FROM "Order" AS o
+    JOIN "Shipper" AS s
+    ON o."ShipVia" = s."Id"
+    WHERE o."OrderDate" < date('2012-08-09');
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
