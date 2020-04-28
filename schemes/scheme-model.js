f317e2210437ad1a0afeffc97ae81534f6d32e54
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-28 17:30:28 -0600

    Added a config.js file and imported it into scheme-model.js. Began setup for making the functions in scheme-model.js.

---
+++ schemes/scheme-model.js
@@ -0,0 +1,35 @@
+const db = require("../data/config")
+
+function find(){
+
+}
+
+function findByID(id){
+
+}
+
+function findSteps(id){
+
+}
+
+function add(scheme){
+
+}
+
+function update(changes, id){
+
+}
+
+function remove(id){
+
+}
+
+
+module.exports = {
+    find,
+    findByID,
+    findSteps,
+    add,
+    update,
+    remove
+}
